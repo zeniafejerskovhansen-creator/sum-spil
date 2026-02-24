e601429644fdae7555dc47fa574f34c7f3b83cd1
zeniafejerskovhansen-creator <zeniafejerskovhansen@gmail.com>
2026-02-24 13:52:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Sum += 1
+    basic.showNumber(Sum)
+})
+input.onButtonPressed(Button.AB, function () {
+    Sum += -3
+    basic.showNumber(Sum)
+})
+input.onButtonPressed(Button.B, function () {
+    Sum += 2
+    basic.showNumber(Sum)
+})
+let Sum = 0
+Sum = 0
+basic.showNumber(0)
